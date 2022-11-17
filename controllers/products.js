9b0c531be35b3e46d751762c6a77ba56ed778ae5
cronugs <cronugs@outlook.com>
2022-11-17 16:19:08 +1100

    add path for 404

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product');
 
-exports.getAddProduct = (req, res, next) => {
+exports.getAddProduct = (_, res) => {
   res.render('add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
@@ -10,13 +10,13 @@ exports.getAddProduct = (req, res, next) => {
   });
 };
 
-exports.postAddProduct = (req, res, next) => {
+exports.postAddProduct = (req, res) => {
   const product = new Product(req.body.title);
   product.save();
   res.redirect('/');
 };
 
-exports.getProducts = (req, res, next) => {
+exports.getProducts = (_, res) => {
   Product.fetchAll(products => {
     res.render('shop', {
       prods: products,
