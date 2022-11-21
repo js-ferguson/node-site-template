48974876feabd6f0351a67b7c17acbb65bcb490b
cronugs <cronugs@outlook.com>
2022-11-21 14:11:31 +1100

    make sure all products have a unique id and add routes and views for details

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,4 @@
-const Product = require('../models/product');
+const Product = require('../models/product')
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -6,9 +6,20 @@ exports.getProducts = (req, res, next) => {
       prods: products,
       pageTitle: 'All Products',
       path: '/products'
-    });
-  });
-};
+    })
+  })
+}
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product,
+      pageTitle: product.title,
+      path: '/products'
+    })
+  })
+}
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -16,27 +27,27 @@ exports.getIndex = (req, res, next) => {
       prods: products,
       pageTitle: 'Shop',
       path: '/'
-    });
-  });
-};
+    })
+  })
+}
 
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     path: '/cart',
     pageTitle: 'Your Cart'
-  });
-};
+  })
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
     pageTitle: 'Your Orders'
-  });
-};
+  })
+}
 
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     path: '/checkout',
     pageTitle: 'Checkout'
-  });
-};
+  })
+}
